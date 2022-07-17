e02223ce8612b0e7c228fd5683425a4e0e69766a
Arkashu <kopeikaarkashu@gmail.com>
2022-07-17 13:42:03 +0300

    add check for 1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict'
 let num = +prompt('Введите любое число');
 for (;;) {
-    if (num < 0 || isNaN(num) ||num % 2 === 0 && num !== 2 || num % 3 === 0 && num !== 3
+    if (num < 0 || isNaN(num) || num === 1 || num % 2 === 0 && num !== 2 || num % 3 === 0 && num !== 3
         || num % 5 === 0 && num !== 5 || num % 7 === 0 && num !==7){
         console.log(false)
     } else {
