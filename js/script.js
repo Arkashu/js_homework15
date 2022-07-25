a21c385d899fe27c37da376579f8b012d1e0184f
Arkashu <kopeikaarkashu@gmail.com>
2022-07-25 15:24:39 +0300

    fixed some details

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 'use strict'
 let num = +prompt('Введите любое число');
-for (;;) {
-    if (num < 0 || isNaN(num) || num === 1 || num % 2 === 0 && num !== 2 || num % 3 === 0 && num !== 3
-        || num % 5 === 0 && num !== 5 || num % 7 === 0 && num !==7){
+for (let i = 0; i < 1; i++) {
+    if (num === 1 ||num ===0 || num < 0 || isNaN(num)) {
         console.log(false)
-    } else {
+        break;
+    } if (num % 2 === 0 && num > 2) {
+        console.log(false)
+        break;
+    } if (num % 3 === 0 && num > 3) {
+        console.log(false)
+        break;
+    } if (num % 5 === 0 && num > 5) console.log(false);
         console.log(true)
-    }
-    break;
 }
